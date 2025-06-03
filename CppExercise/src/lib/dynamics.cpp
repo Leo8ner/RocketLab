4387122fe10bb7ec6a381524af491f54a2619898
Leonardo Eitner <leo.eitner@gmail.com>
2025-06-03 19:14:21 +0200

    cpp is working

---
+++ CppExercise/src/lib/dynamics.cpp
@@ -0,0 +1,72 @@
+#include <rlab/dynamics.h>
+
+using namespace casadi;
+
+// Constructor implementation
+ExplicitDynamics::ExplicitDynamics() {
+    X = SX::vertcat({SX::sym("q", 4), SX::sym("w", 3)});
+    U = SX::sym("tau", 3);
+    dt = SX::sym("dt");
+
+    SX q = X(Slice(0, 4));
+    SX w = X(Slice(4, 7));
+
+    SX S = skew4(w);
+    SX q_dot = 0.5 * SX::mtimes(S, q);
+
+    SX I = SX::diag(SX::vertcat({i_x, i_y, i_z}));
+    SX I_inv = SX::diag(SX::vertcat({1.0/i_x, 1.0/i_y, 1.0/i_z}));
+    SX w_dot = SX::mtimes(I_inv, (U - cross(w, SX::mtimes(I, w))));
+
+    SX X_dot = SX::vertcat({q_dot, w_dot});
+    SX X_next = rk4(X_dot, X, dt);
+    F = Function("F", {X, U, dt}, {X_next});
+
+}
+
+// Skew-symmetric matrix
+SX skew4(const SX& w) {
+    SX S = SX::zeros(4, 4);
+    S(0,1) = -w(0); S(0,2) = -w(1); S(0,3) = -w(2);
+    S(1,0) =  w(0); S(1,2) =  w(2); S(1,3) = -w(1);
+    S(2,0) =  w(1); S(2,1) = -w(2); S(2,3) =  w(0);
+    S(3,0) =  w(2); S(3,1) =  w(1); S(3,2) = -w(0);
+    return S;
+}
+
+// RK4 integrator
+SX rk4(const SX& x_dot, const SX& x, const SX& dt) {
+    SX k1{x_dot};
+    SX k2{substitute(x_dot, x, x + dt / 2 * k1)};
+    SX k3{substitute(x_dot, x, x + dt / 2 * k2)};
+    SX k4{substitute(x_dot, x, x + dt * k3)};
+    return x + dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4);
+}
+
+ImplicitDynamics::ImplicitDynamics() {
+    SX X = SX::vertcat({SX::sym("q", 4), SX::sym("w", 3)});
+    SX U = SX::sym("tau", 3);
+    SX dt = SX::sym("dt");
+
+    SX q = X(Slice(0, 4));
+    SX w = X(Slice(4, 7));
+
+    SX S = skew4(w);
+    SX q_dot = 0.5 * SX::mtimes(S, q);
+
+    SX I = SX::diag(SX::vertcat({i_x, i_y, i_z}));
+    SX I_inv = SX::diag(SX::vertcat({1.0/i_x, 1.0/i_y, 1.0/i_z}));
+    SX w_dot = SX::mtimes(I_inv, (U - cross(w, SX::mtimes(I, w))));
+
+    SX X_dot = SX::vertcat({q_dot, w_dot});
+
+    // Create integrator options
+    SXDict dae = {{"x", X}, {"u", U}, {"p", dt}, {"ode", X_dot*dt}};
+    Dict opts;
+    opts["collocation_scheme"] = "legendre";
+    opts["interpolation_order"] = 3;
+    opts["simplify"] = true;
+    opts["rootfinder"] = "fast_newton";
+    Function f = integrator("f", "collocation", dae, opts);
+    F = f.map(n_stp, "serial");
+}
---
+++ CppExercise/src/lib/optimizer.cpp
@@ -0,0 +1,123 @@
+#include <rlab/optimizer.h>
+
+using namespace casadi;
+
+Function get_solver() {
+        // library prefix and full name
+        std::string prefix_lib = std::filesystem::current_path().parent_path().string() + "/build/";
+        std::string lib_full_name = prefix_lib + "lib_solver.so";
+
+        // use this function
+        return external("solver", lib_full_name);
+}
+
+Optimizer::Optimizer(const Function& dyn, const Constraints& cons) :
+    F(dyn), lb_U(cons.lb_U), ub_U(cons.ub_U), lb_dt(cons.lb_dt), ub_dt(cons.ub_dt),
+    X_0(cons.X_0), X_f(cons.X_f) {
+
+    // Decision variables
+    X = opti.variable(n_X, n_stp + 1); // State trajectory    
+    U = opti.variable(n_U, n_stp);     // Control trajectory (torque)
+    dt = opti.variable(n_stp);               // Time horizon
+
+    // Parameters
+    p_X0 = opti.parameter(n_X);           // Parameter for initial state
+    p_Xf = opti.parameter(n_X);           // Parameter for final state
+    
+    //// Consraints ////
+
+    // Box constraints
+    opti.subject_to(dt>0);    // Time step constraints
+    opti.subject_to(opti.bounded(lb_U, U, ub_U));       // Control constraints
+
+    // Dynamics constraints
+    opti.subject_to(sum1(pow(X(Slice(0,4),all), 2)) == 1); // Ensure |q| = 1
+    MX X_kp1 = F(MXDict{{"x0", X(all,Slice(0, n_stp))}, {"u", U}, {"p", dt}}).at("xf");
+    opti.subject_to(X(all,Slice(1, n_stp+1)) == X_kp1); // Enforce the discretized dynamics
+
+    // Initial and final state constraints
+    opti.subject_to(X(all,0) == p_X0);     // Initial condition
+    opti.subject_to(X(all,n_stp) == p_Xf); // Final condition
+
+    //// Initial guess ////
+    opti.set_initial(dt, dt_0*DM::ones(n_stp)); // Initial guess for the time horizon
+    opti.set_initial(X, stateInterpolator(X_0, X_f, n_stp+1)); // Initial guess for the initial state
+    opti.set_initial(U, inputInterpolator(X_0(Slice(1,4)), X_f(Slice(1,4)), n_stp)); // Initial guess for the control input
+
+    //// Objective ////
+    MX T = sum(dt); // Objective function
+    opti.minimize(T);
+
+    ///// Solver ////
+    Dict plugin_opts{}, solver_opts{};
+    opti.solver("ipopt", plugin_opts, solver_opts); // Set numerical backend
+
+    solver = opti.to_function("solver",
+        {p_X0, p_Xf},                      // Inputs
+        {X, U, T, dt},                        // Outputs
+        {"X0", "Xf"},                      // Input names
+        {"X", "U", "T", "dt"}                   // Output names
+    );
+}
+
+DM stateInterpolator(const DM& x0, const DM& xf, int n_steps) {
+    if (x0.size1() != xf.size1() || x0.size2() != xf.size2()) {
+        throw std::invalid_argument("x0 and xf must have the same dimensions.");
+    }
+    if (n_steps < 2) {
+        throw std::invalid_argument("Number of steps must be at least 2.");
+    }
+
+    int n = 4; 
+    DM q(n, n_steps);
+    Slice all;
+
+    for (int i = 0; i < n_steps; ++i) {
+        double alpha = static_cast<double>(i) / (n_steps - 1);
+        q(all, i) = (1.0 - alpha) * x0(Slice(0, n)) + alpha * xf(Slice(0, n));
+    }
+    DM omega = ratesInterpolator(x0(Slice(1, n)), xf(Slice(1, n)), n_steps);
+
+    DM result = DM::vertcat({q, omega});
+
+    return result;
+}
+
+DM ratesInterpolator(const auto& x0, const auto& xf, int n_steps) {
+    if (x0.size1() != xf.size1() || x0.size2() != xf.size2()) {
+        throw std::invalid_argument("x0 and xf must have the same dimensions.");
+    }
+    if (n_steps < 2) {
+        throw std::invalid_argument("Number of steps must be at least 2.");
+    }
+
+    int n = x0.size1(); 
+    DM result(n, n_steps);
+    Slice all;
+    auto sgn = sign(xf - x0); // Sign function to handle direction
+    for (int i = 0; i < n_steps/2; ++i) {
+        double alpha = static_cast<double>(i) / (n_steps/2 - 1);
+        result(all, i) = sgn * alpha;
+        result(all, n_steps - 1 - i) = result(all, i);
+    }
+
+    return result;
+}
+
+DM inputInterpolator(const auto& x0, const auto& xf, int n_steps) {
+    if (x0.size1() != xf.size1() || x0.size2() != xf.size2()) {
+        throw std::invalid_argument("x0 and xf must have the same dimensions.");
+    }
+    if (n_steps < 2) {
+        throw std::invalid_argument("Number of steps must be at least 2.");
+    }
+
+    int n = x0.size1(); 
+    DM result(n, n_steps);
+    Slice all;
+
+    result(all, Slice(0, n_steps/2)) = repmat(sign(xf-x0), 1, n_steps/2);
+    result(all, Slice(n_steps/2, n_steps)) = repmat(-sign(xf-x0), 1, ceil(n_steps/2.0));
+
+    return result;
+}
